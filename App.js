3aeb39ae2322bf30021b1e5a08e2ab11cc0623ee
aeflorez9707 <andresflorez9707@gmail.com>
2019-09-22 23:17:16 -0500

    Second commit

--- App.js
+++ App.js
@@ -1,63 +1,23 @@
-
-/**
- * Store - holds our state - THERE IS ONLY ONE STATE 
- * Action - State can be modified using actions - SIMPLE OBJECTS 
- * Dispatcher - Action needs to be sent by someone - known as dispatching an action
- * Reducer - receives the action and modifies the state to give us a new state 
- *  - pure functions 
- *  - only mandatory argument is the 'type' 
- * Subscriber - listens for state change to update the ui  
- */
-
-
 import React from 'react';
-import { Platform,View,Text,StyleSheet,TouchableOpacity } from 'react-native';
-import { AppLoading, Font } from 'expo';
-import { createStore } from 'redux'
-import CounterApp from './src/CounterApp'
+import { StyleSheet, Text, View } from 'react-native';
+import TodoApp from './src/TodoApp'
+import store from './src/store'
 import { Provider } from 'react-redux'
-
-const initialState = {
-    counter: 0
-}
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INCREASE_COUNTER':
-            return { counter: state.counter + 1 }
-        case 'DECREASE_COUNTER':
-            return { counter: state.counter - 1 }
-    }
-    return state
-}
-
- const store = createStore(reducer)
-
 export default class App extends React.Component {
- /* constructor(){
-    super()
-
-    this.state = {
-      counter:0
-    }
-}
- */
   render() {
-    
-      
-        return (
-            <Provider store={store}>
-                <CounterApp />
-            </Provider>
-        );
-      
-      }
-    
+    return (
+      <Provider store={store}>
+        <TodoApp />
+      </Provider>
+    );
   }
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity
+} from "react-native";  
+import { Ionicons } from '@expo/vector-icons'
+import {connect} from 'react-redux'
+import {addTodo} from '../actions'
+class AddTodo extends Component {
+    state={
+       text:'' 
+    }
+    AddTodo=(text)=>{
+        this.props.dispatch(addTodo(text))
+        this.setState({text:''})
+    }
+    render() {
+        return (
+            <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
+                <TextInput
+                    value={this.state.text}
+                    onChangeText={(text)=>this.setState({text})}
+                    placeholder="Eg. Create New Video"
+                    style={{ borderWidth: 1, borderColor: '#f2f2e1', backgroundColor: '#eaeaea', height: 50, flex: 1, padding: 5 }}
+                />
+                <TouchableOpacity onPress={() => this.AddTodo(this.state.text)}>
+                    <View style={{ height: 50, backgroundColor: '#eaeaea', alignItems: 'center', justifyContent: 'center' }}>
+                        <Ionicons name="md-add" size={30} style={{ color: '#de9595', padding: 10 }} />
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+export default connect()(AddTodo);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
